58aea224a39a27f23eba6a5f8fb58db555817ae3
karamach <princekarthik@gmail.com>
2017-10-17 02:48:20 -0400

    fix comment

--- finding_lanelines/src/lane_detection.py
+++ finding_lanelines/src/lane_detection.py
@@ -35,22 +35,6 @@ class LaneDetector(object):
 
     
     def draw_lines(self, img, lines, color=[255, 0, 0], thickness=5):
-        """
-        NOTE: this is the function you might want to use as a starting point once you want to 
-        average/extrapolate the line segments you detect to map out the full
-        extent of the lane (going from the result shown in raw-lines-example.mp4
-        to that shown in P1_example.mp4).  
-    
-        Think about things like separating line segments by their 
-        slope ((y2-y1)/(x2-x1)) to decide which segments are part of the left
-        line vs. the right line.  Then, you can average the position of each of 
-        the lines and extrapolate to the top and bottom of the lane.
-    
-        This function draws `lines` with `color` and `thickness`.    
-        Lines are drawn on the image inplace (mutates the image).
-        If you want to make the lines semi-transparent, think about combining
-        this function with the weighted_img() function below
-        """
         ll_slope_sum, ll_cnt, rl_slope_sum, rl_cnt = 0, 0, 0, 0
         ll_max, rl_max = (img.shape[1], img.shape[0]), (img.shape[1], img.shape[0])
 
